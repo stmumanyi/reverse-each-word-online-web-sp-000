aa0ab5042c5b000ab6790a1d09b8a080f4cb2750
Simba Mumanyi <simbamumanyi@gmail.com>
2020-07-15 14:44:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(array)
   fresh_array = array.split(" ")
   reversed_array = fresh_array.collect {|x| x.reverse!}
-  return reversed_array.join
+  return reversed_array.join(" ")
 
 end
 
